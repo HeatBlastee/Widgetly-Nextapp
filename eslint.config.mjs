9010c7311a79ae03e7319b14101a393a027c80fe
HeatBlastee <mann42006@gmail.com>
2025-06-16 13:11:00 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off", // disable globally
+      "@typescript-eslint/no-explicit-any": "off",
     },
   },
 ];
